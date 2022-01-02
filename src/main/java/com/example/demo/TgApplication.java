1f20ec8207432d7cd0bda9c37d459fe1e026a0d6
Luis <Luis@DESKTOP-MK4RUFD>
2022-01-01 20:36:25 -0500

    holo

---
+++ src/main/java/com/example/demo/TgApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgApplication.class, args);
+		
+		System.out.println("holo");
+	}
+
+}
